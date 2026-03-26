43d67325a185279d4882ff10705a733a42a6f505
Loic Gerard <lgerard@diatem.net>
2026-03-26 14:39:40 +0100

    Upd

--- api/app.py
+++ api/app.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5000", "http://localhost:5000"],
+    allow_origins=["http://127.0.0.1:5000", "http://localhost:5000", "http://193.168.144.145:5000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
